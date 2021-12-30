71eb86b21b6a0002bb8c01a7fff5cfd8804ae5fe
marahsaadeh <92647123+marahsaadeh@users.noreply.github.com>
2021-12-30 12:57:07 +0200

    Create m-code1.js

---
+++ m-code1.js
@@ -0,0 +1,6 @@
+# m-code1
+function removeTodo(index) {
+  const todos = JSON.parse(localStorage.getItem("todos"));
+  todos.splice(index, 1);
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
